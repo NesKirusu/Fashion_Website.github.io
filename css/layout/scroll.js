05d5b2f14767280da14d27cadc03156512bfb8b0
Christian Barretto <angkevin33@gmail.com>
2025-03-18 19:13:48 +0800

    2.6

--- css/layout/scroll.js
+++ css/layout/scroll.js
@@ -5,20 +5,8 @@ const lenis = new Lenis({
   direction: 'vertical', // Scroll direction: vertical or horizontal
   gestureDirection: 'vertical', // Gesture direction: vertical, horizontal, or both
   smooth: true, // Enable smooth scrolling
-  mouseMultiplier: 2, // Mouse scroll speed multiplier
+  mouseMultiplier: 1, // Mouse scroll speed multiplier
   smoothTouch: false, // Disable smooth scrolling for touch devices
   touchMultiplier: 2, // Touch scroll speed multiplier
   infinite: false, // Disable infinite scroll
 });
-
-// Optional: Listen to scroll events
-lenis.on('scroll', ({ scroll, limit, velocity, direction, progress }) => {
-  console.log({ scroll, limit, velocity, direction, progress });
-});
-
-// Animation frame loop function
-function raf(time) {
-  lenis.raf(time);
-  requestAnimationFrame(raf);
-}
-requestAnimationFrame(raf);
